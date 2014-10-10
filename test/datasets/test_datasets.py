72510c3b2f19e85f5421dd8779bfef856bb694a5
cokelaer <cokelaer@gmail.com>
2014-10-10 15:38:30 +0100

    update tests

--- test/datasets/test_datasets.py
+++ test/datasets/test_datasets.py
@@ -1,9 +1,22 @@
+import os
 from cno.datasets import ToyMMB
+from cno.datasets import cnodata, registers
 
 
+def test_registered_cnodata():
+    for filename in registers:
+        fullpath = cnodata(filename)
+        assert os.path.exists(fullpath)
+
+    try:
+        cnodata('dummy')
+        assert False
+    except:
+        assert True
 
 def test_datasets_toymmb():
 
-    ToyMMB.data.df
-    ToyMMB.model
+    ToyMMB.model_filename
+    ToyMMB.data_filename
     ToyMMB.description
+    ToyMMB.plot()
