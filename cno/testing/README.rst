7a74cfc9d4989a84706d385f2a8d1c415a334be4
cokelaer <cokelaer@gmail.com>
2014-11-03 17:23:28 +0000

    updating the testing directory with new files.

---
+++ cno/testing/README.rst
@@ -0,0 +1,97 @@
+data sets for testing purposes. 
+==================================
+
+
+MIDAS related
+---------------
+
+- MD-MultipleCellLines.csv : a MIDAS file wit several cell lines
+- MD-Test.csv: a general MIDAS file
+
+Graph/SIF related
+------------------
+
+- PKN-ToyMMB.net
+- test_adjacency_matrix.csv used by adj2sif
+- test_adjacency_names.csv  used by adj2sif
+- test_cna_reactions
+- test_data.sop used by sop2sif
+- test_double.sif
+- test_simple.eda
+- test_simple.sif
+
+
+todo
+-----
+
+caspo_models.csv
+kinetic.csv
+MD-average.csv
+MD-one_exp.csv
+MD-one_inhibitor.csv
+MD-one_stimulus.csv
+MD-test_1cue_noinhibitor.csv
+MD-Test2.csv
+MD-test_4andgates.csv
+MD-test_diamond.csv
+MD-ToyMMB.csv
+MD-unnorm.csv
+MD-unnorm_exp.csv
+MD-unnorm_exp_nozero.csv
+midas.xml
+PKN-test_1cue_noinhibitor.sif
+PKN-test_4andgates.sif
+PKN-TestAND.sif
+PKN-test_diamond.sif
+PKN-Test.sif
+PKN-ToyMMB.sif
+README.txt
+test_sbml.xml
+
+
+======================= ======================================
+filename                used by 
+======================= ======================================
+kinetic.csv             kinexus
+MD-ToyMMB.csv           general
+PKN-ToyMMB.net          sif2asp
+PKN-ToyMMB.sif          general
+reactions               Reactions
+test.sif
+MD-one_exp              MIDAS with single experiment
+MD-one_stimulus         MIDAS with single stimuli
+MD-one_inhiibitor       MIDAS special case  
+MD-unnorm.csv           MIDAS to test normalisation(time)
+MD-unnorm_exp.csv       MIDAS to test normalisation(exp)
+MD-Test.csv             Same as MD-ToyPB with NA
+MD-Test2.csv            same as aboe with 2 cell lines
+PKN-Test.sif            same as PKN-ToyPB but with nonc
+MD-average.csv          test average over time
+======================= ======================================
+
+
+- test_metabolites used by Metabolites
+
+metabolites_samples:
+----------------------
+    ExampleNetwIdent
+    ExampleNetwIdentNoCrossT
+    FasTnfTrail
+    FusedLDPSN
+    HepatHCCs
+    LDPSN-DREAM
+    LDPSN-DREAM-simpler
+    LDPSN-Ingenuity
+    LDPSN-IngenuityFourHCC
+    LDPSN-IngenuityNeg
+    LDPSN-Ingenuity-NoNeg
+    LDPSN-Ingenuity-NoNeg-Extend
+    LDPSN-Ingenuity-NoNeg-Extend1b
+    LDPSN-Ingenuity-NoNegIns
+    LDPSN-Ingenuity-NoNegIns4Rec
+    LDPSN-IngenuityOldMod
+    SchlatterApopt
+    sigtoynet
+    TcellReduced
+    TcellSmall
+    U937Signaling
--- cno/testing/__init__.py
+++ cno/testing/__init__.py
@@ -1,20 +1,67 @@
 #from __future__ import absolute_import
 
 import os
+import glob
+
 from . import __path__
+
 from cno.misc import CNOError
 
-__all__ = ['getdata']
+__all__ = ['getdata', 'getdata_metabolites']
 
 
-def getdata(filename):
-    filename = __path__[0] + os.sep + filename
-    if os.path.exists(filename):
+def getdata(filename=None, pattern=None, verbose=True, raise_error=True):
+    """Return the fullpath of a filename to be found in the test data set
+
+    cellnopt contains a directory called cno/testing that provides
+    data sets used within the test suite, which can also be multi-purpose within
+    notebooks or demonstration.
+
+    This function can be used without argument to figure out the
+    list of available files. Otherwise, it tries to return the fullpath. If not
+    found, a CNOError is returned.
+
+    If filename is incorrect, the list of valid names is printed. To prevent
+    that behaviour, you can set the **verbose** parameter to False.
+
+    if pattern provided, raise_error is sset to False and list of
+    filenames that contain the pattern is returned
+    """
+    if filename:
+        filename = __path__[0] + os.sep + filename
+    if filename and os.path.exists(filename):
         return filename
     else:
         import glob
         valid_filenames = glob.glob(__path__[0] + os.sep + '*')
         txt = "Unknown file {0}\n Valid files are:\n".format(filename)
         for this in valid_filenames:
-            txt += " - {0}\n".format(this)
-        raise CNOError(txt)
+            if this.endswith("pyc") or this.endswith('.py'):
+                continue
+            if os.path.isdir(this):
+                continue
+            this_end = this.split("cno" + os.sep + "testing")[1][1:]
+            txt += " - {0}\n".format(this_end)
+
+        metabolites = getdata_metabolites()
+        for this in metabolites:
+            this_end = this.split("cno" + os.sep + "testing")[1][1:]
+            txt += " - {0}\n".format(this_end)
+
+        print(txt)
+        if pattern:
+            raise_error = False
+            valid_filenames = [filename for filename in valid_filenames
+            if pattern.replace("*", "") in filename]
+        if raise_error:
+            raise CNOError("Incorrect filename. See list of valid filenames above.")
+        else:
+            return valid_filenames
+
+
+def getdata_metabolites():
+    metabolites = glob.glob(__path__[0] + os.sep
+                + 'metabolites_samples' + os.sep + '*')
+    return metabolites
+
+
