e34d2ac506420537495017e25a3545d9a33848a6
cokelaer <cokelaer@gmail.com>
2014-11-07 17:29:44 +0000

    update notebooks

--- test/io/test_midas.py
+++ test/io/test_midas.py
@@ -9,34 +9,46 @@ from cno import cnodata
 from easydev.easytest import assert_list_almost_equal, TempFile
 
 from cno.testing import getdata
-
+from cno.misc import CNOError
 
 #filenames = ['MD-ToyMMB_bis.csv',
 #             'MD-LiverDREAM.csv']
 
 filenames = getdata(pattern="MD*")
 
-def _test_reading_all_file():
-    # this is a nosetests trick to have one test per file reportig in the output
-    # and not just one for all files. This way, we now how many files are tested
-    for filename in filenames:
-        yield readfiles, filename
 
-def readfiles(filename):
-    if 'multiple' in filename:
-        m = XMIDAS(filename, cellLine='C1')
-    else:
-        m = XMIDAS(filename)
-
+# Trying
 def test_reading_and_saving():
     for filename in filenames:
         yield reading_and_saving, filename
 
 def reading_and_saving(filename):
+    # if multiple cell line, we must provide the cell line name
+
     if 'multiple' in filename:
         m1 = XMIDAS(filename, cellLine='C1')
+        try:
+            m1 = XMIDAS(filename)
+            assert False
+        except:
+            assert True
+
+
+    # if wrong MIDAS, a CNOError should be raised
+    elif 'wrong' in filename:
+        try:
+            m1 = XMIDAS(filename)
+            assert False
+        except CNOError:
+            assert True
+            return
+        except:
+            assert False
     else:
         m1 = XMIDAS(filename)
+
+    # we can also test the following: writing the XMIDAS into a file (MIDAS
+    # format) and read it back. the 2 objects must be identical !
     f = TempFile()
     m1.save2midas(f.name)
     m2 = XMIDAS(f.name)
@@ -45,64 +57,42 @@ def reading_and_saving(filename):
 
     print(m1)
 
+def test_read_xmidas_nofilename():
+    m = XMIDAS()
+    m.cellLine
 
+def test_no_stimuli():
+    m = XMIDAS(getdata("MD-test_one_stimulus.csv"))
+    m.remove_stimuli('egf')
+    m.names_stimuli
 
-
-"""
-def test_multi_cellline():
-    c = XMIDAS(getdata("MD-MultipleCellLines.csv"), cellLine="C1")
-
-
-def _test_compare_pycno_vs_cnor():
-    # test all sif files 
-    for f in filenames:
-        yield compare_pycno_vs_cnor, f
-    
-    
-def _compare_pycno_vs_cnor(filename):
-    #check that the sif file read by CNOR and pyCNO are identical
-    filename = cnodata(filename)
-    try:
-        from cellnopt.wrapper import rpack_CNOR as cnor
-        from cellnopt.core import midas
- 
-        mr = cnor.readMIDAS(filename)
-        m = midas.MIDASReader(filename)
-
-        assert sorted(list(mr.rx2('DAcol'))) == sorted([x+1 for x in list(m.DAcol)])
-        assert sorted(list(mr.rx2('DVcol'))) == sorted([x+1 for x in list(m.DVcol)])
-        assert sorted(list(mr.rx2('TRcol'))) == sorted([x+1 for x in list(m.TRcol)])
-    except:
-        pass
-
-    # TODO(tc): seems that we need to transpose one matrix. do not seem neat
-    #assert numpy.array(m.dataMatrix).all() == numpy.array(mr.dataMatrix).transpose().all()
-
-
-
-
-
-
-
+def test_no_inhibitors():
+    m = XMIDAS(getdata("MD-test_one_stimulus.csv"))
+    m.remove_inhibitors(['pi3k', 'raf1'])
+    m.names_inhibitors
 
 
 def test_xmidas():
+    # constructor
     m = XMIDAS(cnodata("MD-ToyPB.csv"))
+    m2 = XMIDAS(m)
+    assert m == m2
+
+    # accessors
     m.df
     assert len(m.names_cues)==4
-
     assert len(m.experiments) == 10
-
     assert len(m.names_stimuli)==2
     assert len(m.stimuli)==10
-
     assert len(m.inhibitors) == 10
     assert len(m.names_inhibitors) == 2
-
-    m.cellLine
+    assert m.cellLine == 'Cell'
     assert len(m.cellLines) == 1
     assert len(m.times ) == 16
 
+    # getter
+    assert m['p38'].shape == (160,)
+    assert m['Cell','experiment_0', 0].shape == (6,)
     try:
         m.cellLine = "wrong"
         assert False
@@ -122,7 +112,11 @@ def test_xmidas():
     m.remove_times([12,14])
     m.remove_experiments("experiment_0")
     m.remove_stimuli("p38")
-    m.remove_inhibitors("pi3k:i")
+    m.remove_inhibitors("pi3k")
+
+    m.remove_inhibitors("dummy")
+    m.remove_stimuli("dummy")
+
 
     mm = m.copy()
     mm.remove_cellLine("Cell")
@@ -131,20 +125,110 @@ def test_xmidas():
     # ------------------------------------------- rename methods
     m.rename_cellLine({"Cell": "HT29"})
     m.rename_species({"ap1":'AP1'})
-    m.rename_inhibitors({'raf1:i':'RAF1'})
+    m.rename_inhibitors({'raf1':'RAF1'})
     m.rename_stimuli({'egf':'EGF'})
+    m.rename_time({0:0,2:2*60,4:4*60})
 
     # others
-    print(m)
     m.get_residual_errors()
 
-
-    m.rename_time({0:0,2:2*60,4:4*60}) 
-
 def test_xmidas_average():
-    m = XMIDAS(getdata("MD-average.csv"))
+    m = XMIDAS(getdata("MD-test_average.csv"))
     m.average_replicates()
     m.average_replicates(inplace=True)
+
+def test_operators():
+    m = XMIDAS(cnodata("MD-ToyPB.csv"))
+    # test *, +, and / operators
+    assert (m + m) * 0.25 == m / 2
+    assert (m - 0.5) + 0.5 == m
+    assert (m - m).df.sum().sum() == 0
+
+
+#plotting
+
+
+
+
+def test_xmidas_corr():
+    m = XMIDAS(cnodata("MD-ToyPB.csv"))
+    m.corr()
+
+
+# scaling
+def test_scaling():
+    m = XMIDAS(cnodata("MD-ToyMMB.csv"))
+    m.scale_max_by_experiments()
+    assert (m.df == 1).sum().sum() == 40
+
+    m = XMIDAS(m)
+    m.scale_max()
+    assert (m.df == 1).sum().sum() == 40
+
+
+    m = XMIDAS(m)
+    m.scale_min_max()
+
+    m = XMIDAS(m)
+    m.scale_max_by_experiments()
+
+    m = XMIDAS(m)
+    m.scale_min_max_by_experiments()
+
+    m = XMIDAS(m)
+    m.scale_max_across_experiments()
+
+    m = XMIDAS(m)
+    m.scale_min_max_across_experiments()
+
+    # same but with inplace set to False
+    m = XMIDAS(m)
+    m.scale_max(inplace=False)
+    m = XMIDAS(m)
+    m.scale_min_max(inplace=False)
+    m = XMIDAS(m)
+    m.scale_max_by_experiments(inplace=False)
+    m = XMIDAS(m)
+    m.scale_min_max_by_experiments(inplace=False)
+    m = XMIDAS(m)
+    m.scale_max_across_experiments(inplace=False)
+    m = XMIDAS(m)
+    m.scale_min_max_across_experiments(inplace=False)
+
+
+"""
+
+
+def _test_compare_pycno_vs_cnor():
+    # test all sif files
+    for f in filenames:
+        yield compare_pycno_vs_cnor, f
+
+
+def _compare_pycno_vs_cnor(filename):
+    #check that the sif file read by CNOR and pyCNO are identical
+    filename = cnodata(filename)
+    try:
+        from cellnopt.wrapper import rpack_CNOR as cnor
+        from cellnopt.core import midas
+
+        mr = cnor.readMIDAS(filename)
+        m = midas.MIDASReader(filename)
+
+        assert sorted(list(mr.rx2('DAcol'))) == sorted([x+1 for x in list(m.DAcol)])
+        assert sorted(list(mr.rx2('DVcol'))) == sorted([x+1 for x in list(m.DVcol)])
+        assert sorted(list(mr.rx2('TRcol'))) == sorted([x+1 for x in list(m.TRcol)])
+    except:
+        pass
+
+    # TODO(tc): seems that we need to transpose one matrix. do not seem neat
+    #assert numpy.array(m.dataMatrix).all() == numpy.array(mr.dataMatrix).transpose().all()
+
+
+
+
+
+
 
 
 
@@ -186,11 +270,8 @@ def test_heatmap():
 def test_hcluster():
     m = XMIDAS(cnodata("MD-ToyPB.csv"))
     m.hcluster()
-    
 
-def test_xmidas_corr():
-    m = XMIDAS(cnodata("MD-ToyPB.csv"))
-    m.corr()
+
 
 def test_xmidas_experiments():
     m = XMIDAS(cnodata("MD-ToyPB.csv"))
