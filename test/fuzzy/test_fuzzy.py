4cc81296a7e78c46dec1f446bfde717c2599d043
cokelaer <cokelaer@gmail.com>
2014-12-04 14:53:07 +0000

    add test for fuzzy case

---
+++ test/fuzzy/test_fuzzy.py
@@ -0,0 +1,13 @@
+from cno import CNORfuzzy, cnodata
+
+
+
+def test_fuzzy():
+    c = CNORfuzzy(cnodata("PKN-ToyMMB.sif"), cnodata("MD-ToyMMB.csv"))
+    c.optimise(2, maxgens=10)
+    score = c.simulate([ 3, 3, 3, 0, 4, 5, 0, 4, 5, 5, 6, 5, 2, 7, 6, 5, 0, 1, 0])
+    assert score == 0.0254795668606642
+    c.plotMSE()
+    c.plot_errors()
+    c.plot_fitness()
+
