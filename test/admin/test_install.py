0637a8ae5b0ce44da62fbda30ad6df28c3fbe912
Thomas Cokelaer <cokelaer@gmail.com>
2014-12-21 16:11:38 +0000

    updating tests.

--- test/admin/test_install.py
+++ test/admin/test_install.py
@@ -1,7 +1,11 @@
 from cno.admin import install
-
+import nose
+import logging
+import sys
 
 
 def test_install():
     install.install_all_cellnopt_dependencies(verbose=True)
-    install.__main__()
+
+
+
