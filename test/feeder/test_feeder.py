bc4034fa0d1857e28fcdf1f88b0233e0d7ffb63c
Thomas Cokelaer <cokelaer@gmail.com>
2014-12-21 16:13:42 +0000

    update testswq

--- test/feeder/test_feeder.py
+++ test/feeder/test_feeder.py
@@ -2,16 +2,12 @@ from cno import cnodata
 from cno.feeder import Feeder
 
 
-
 def test_feeder():
-
     f = Feeder()
     pknmodel = cnodata("PKN-ToyMMB.sif")
     midas = cnodata("MD-ToyMMB.csv")
-
     f.run(model=pknmodel, data=midas)
     f.newlinks
     print(f)
-
     f.plot()
 
