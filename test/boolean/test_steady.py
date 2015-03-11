da50a113c40e13d8d801d07efcd77d0d5fe47ed1
cokelaer <cokelaer@gmail.com>
2015-03-11 17:34:58 +0000

    updating test

--- test/boolean/test_steady.py
+++ test/boolean/test_steady.py
@@ -5,26 +5,27 @@ import numpy
 
 def test_steady():
 
+
+    # simulation using all bit strings to 1
     s = Steady(cnodata("PKN-ToyMMB.sif"), cnodata("MD-ToyMMB.csv"))
     s.simulate(1) 
     assert s.score() == 0.094774603174603178
-
-
-    # ???
-    #s = Steady(cnodata("PKN-ToyMMB.sif"), cnodata("MD-ToyMMB.csv"))
-    #s.preprocessing()
-    #reactions = ['!Akt=Mek', '!Akt^Raf=Mek', 'EGF=PI3K', 'Erk=Hsp27',
-    #         'Jnk=cJun','Mek=Erk', 'Mek=p90RSK', 'PI3K=Akt',
-    #            'Raf=Mek',  'TNFa=Hsp27', 'TNFa=Jnk', 'TNFa=NFkB', 'TNFa=PI3K']
-    #s.simulate(reactions)
-    #s.score()/2. == 0.10832539682539682
 
 
     s = Steady(cnodata("PKN-LiverDREAM.sif"), cnodata("MD-LiverDREAM.csv"))
     s.simulate(1) 
     assert s.score() == 0.2574947901818182
 
+    s = Steady(cnodata("PKN-ExtLiverPCB.sif"), cnodata("MD-ExtLiverPCB.csv"))
+    s.simulate(1) 
+    assert s.score() == 0.29198998104079721
 
 
+    # Now test if several reactions are removed
+    s = Steady(cnodata("PKN-ToyMMB.sif"), cnodata("MD-ToyMMB.csv"))
+    s.simulate(reactions=['EGF=Ras'])
+    assert s.score() == 0.16610942460317463
+    s.simulate(reactions=['EGF=Ras', 'EGF=PI3K'])
+    assert s.score() == 0.16611567460317461
 
 
