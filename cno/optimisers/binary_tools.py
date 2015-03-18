7946432b0fce7eb923d71f7cf73f3d27067a72e4
Thomas Cokelaer <cokelaer@gmail.com>
2015-03-18 21:50:56 +0000

    add new module related to boolean/binary strings

---
+++ cno/optimisers/binary_tools.py
@@ -0,0 +1,12 @@
+import itertools
+
+
+
+def perm(L, M=1):
+    """Create all the permutations of a bitstring of length L
+    M : max value of the parameter. For instance to get all combination
+    of 0,1 and 2 of strings of length 10 perm(10, 2)
+    """
+    a = itertools.product(range(0, M+1), repeat=L)
+    return a # this is an iterator.
+
