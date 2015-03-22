d51648591ffdd39d4a58656be53eff6d72829919
Thomas Cokelaer <cokelaer@gmail.com>
2015-03-22 22:27:25 +0000

    starting implementation fuzzy based on steady module

--- cno/fuzzy/fuzzy.py
+++ cno/fuzzy/fuzzy.py
@@ -1,10 +1,340 @@
+from cno.core.base import CNOBase
+from cno.core.results import BooleanResults
+from cno.core.models import BooleanModels
+from cno.misc.profiler import do_profile
+from cno.io.reactions import Reaction
+
+
+import pandas as pd
+import numpy as np
+import pylab
+import time
+import bottleneck as bn
+from collections import defaultdict
+import collections
+
+from cno.boolean.steady import Steady
+
+
+class Fuzzy(Steady):
+    """Implementation of Fuzzy logic
+
+
+    Edge are of 2 types: either edge from a stimuli, or other edges.
+    
+    In original version, parameters are encoded as discrete values [0,1,...8]
+    and those values are mapped onto a set of g,k,n parameters of the hill function.
+
+    Stimuli have only one parameter to tune: the gain. Assume a linear variation. Why ?
+    Others have same gain set to 1 but k and n changes. why ?
+    
+    
+    """
+    
+    def __init__(self, pknmodel, data, verbose=True):
+        super(Fuzzy, self).__init__(pknmodel, data, verbose)
+
+    def init(self, time):
+        super(Fuzzy, self).init(time)
+
+    def hill_tf(self, x, g, n, k):
+        return (g*(1+k**n)*x**n/(x**n+k**n))
+
+
+    #@do_profile()
+    def simulate(self, tick=1, parameters=[]):
+        """
+
+
+        parameters will be a list of params on each edge.
+
+        """
+        # pandas is very convenient but slower than numpy
+        # The dataFrame instanciation is costly as well.
+        # For small models, it has a non-negligeable cost.
+
+        # inhibitors will be changed if not ON
+        #self.tochange = [x for x in self.model.nodes() if x not in self.stimuli_names
+        #            and x not in self.and_gates]
+
+        # what about a species that is both inhibited and measured
+        testVal = 1e-3
+
+        values = self.values.copy()
+
+        if self.debug:
+            self.debug_values = []
+        self.residuals = []
+        self.penalties = []
+
+        self.count = 0
+        self.nSp = len(values)
+        residual = 1.
+
+        frac = 1.2
+        # #FIXME +1 is to have same resrults as in CellnOptR
+        # It means that if due to the cycles, you may not end up with same results.
+        # this happends if you have cyvles with inhbititions
+        # and an odd number of edges. 
+        if reactions is None:
+            reactions = self.model.buffer_reactions
+        self.number_edges = len(reactions)
+
+        # 10 % time here
+        #predecessors = self.reactions_to_predecessors(reactions)
+        predecessors = defaultdict(collections.deque)
+        for r in reactions:
+            k,v = self._reac2pred[r]
+            predecessors[k].extend(v)
+
+        # speed up
+        keys = self.values.keys()
+        length_predecessors = dict([(node, len(predecessors[node])) for node in keys])
+
+        #self._length_predecessors = length_predecessors
+        # if there is an inhibition/drug, the node is 0
+        values = self.values.copy()
+        for inh in self.inhibitors_names:
+            if length_predecessors[inh] == 0:
+                #values[inh] = np.array([np.nan for x in range(0,self.N)])
+                #values[inh] = np.array([0 for x in range(0,self.N)])
+                values[inh] = np.zeros(self.N)
+
+        while (self.count < self.nSp * frac +1.) and residual > testVal: 
+            self.previous = values.copy()
+            #self.X0 = pd.DataFrame(self.values)
+            #self.X0 = self.values.copy()
+            # compute AND gates first. why
+            for node in self.and_gates:
+                # replace na by large number so that min is unchanged
+                # THere are always predecessors
+                if length_predecessors[node] != 0:
+                    values[node] = bn.nanmin(np.array([values[x] for x in predecessors[node]]), axis=0)
+                else:
+                    #assert 1==0, "%s %s" % (node, predecessors[node])
+                    values[node] = self.previous[node]
+
+            for node in self.tochange:
+                # easy one, just the value of predecessors
+                #if len(self.predecessors[node]) == 1:
+                #    self.values[node] = self.values[self.predecessors[node][0]].copy()
+                if length_predecessors[node] == 0:
+                    pass # nothing to change
+                else:
+                    # TODO: if only one input, no need for that, just propagate signal.
+                    dummy = np.array([values[x] if (x,node) not in self.toflip 
+                        else 1 - values[x] for x in  predecessors[node]])
+                    values[node] = bn.nanmax(dummy,  axis=0)
+
+                # take inhibitors into account
+                if node in self.inhibitors_names:
+                    # if inhibitors is on (1), multiply by 0
+                    # if inhibitors is not active, (0), does nothing.
+                    values[node] *= 1 - self.inhibitors[node].values
+            # here NAs are set automatically to zero because of the int16 cast
+            # but it helps speeding up a bit the code by removig needs to take care
+            # of NAs. if we use sumna, na are ignored even when 1 is compared to NA            
+            self.m1 = np.array([self.previous[k] for k in keys ], dtype=np.int16)
+            self.m2 = np.array([values[k] for k in keys ], dtype=np.int16)
+            #residual = bn.nansum(np.square(self.m1 - self.m2))
+            #residual = np.nansum(np.square(self.m1 - self.m2))
+            residual = np.nansum(np.square(self.m1 - self.m2))
+
+
+            # TODO stop criteria should account for the length of the species to the
+            # the node itself so count < nSp should be taken into account whatever is residual.
+            #
+            if self.debug:
+                self.debug_values.append(self.previous.copy())
+           
+            self.residuals.append(residual)
+            self.count += 1
+
+        if self.debug is True:
+            # add the latest values simulated in the while loop
+            self.debug_values.append(values.copy())
+
+        # Need to set undefined values to NAs
+        self.simulated[self.time] = np.array([values[k] 
+            for k in self.data.df.columns ], dtype=float)#.transpose()
+
+        self.prev = {}
+        self.prev[self.time] = np.array([self.previous[k] 
+            for k in self.data.df.columns ], dtype=float)#.transpose()
+
+        mask = self.prev[self.time] != self.simulated[self.time]
+        self.simulated[self.time][mask] = np.nan
+
+        self.simulated[self.time] = self.simulated[self.time].transpose()
+
+        # set the non-resolved bits to NA
+        # TODO TODO TODO
+        #newInput[which(abs(outputPrev-newInput) > testVal)] <- NA
+        # loops are handle diffenty
+
+    #@do_profile()
+    def score(self, NAFac=1, sizeFac=1e-4):
+        # We need also to include NAFac, number of reactions in the model
+        # for the sizeFac
+
+        # time 1 only is taken into account
+        #self.diff = np.square(self.measures[self.time] - self.simulated[self.time])
+        diff = self.measures[self.time] - self.simulated[self.time]
+        diff *= diff
+
+        N = diff.shape[0] * diff.shape[1]
+        Nna = np.isnan(diff).sum()
+        N-= Nna
+
+        #nInTot = number of edges on in global model
+        #nInTot = len(self.model.reactions)
+        nInTot = self.nInputs # should be correct
+        nDataPts = diff.shape[0] * diff.shape[1]
+        nDataP = N # N points excluding the NA if any
+        #print(N)
+
+        #NAPen = NAFac * sum(self.simulated.isnull())
+
+        # nInTot: number of inputs of expanded miodel
+        # nInputs: number of inputs of cut model
+
+        # In CNO:
+        # nDataPts = number of points irrespective of NA
+        # nDataP  sum(!is.na(CNOlist@signals[[timeIndex]]))
+        # nInputs = number of inputs of the cut model
+
+        # for now, ketassume it is the same as the number of reactions
+        # TODO AND gates should count for 1 edge
+        nInputs = self.number_edges
+
+        sizePen = nDataPts * sizeFac * nInputs / float(nInTot)
+        #self.debug("nDataPts=%s" % nDataPts)
+        #self.debug("nInputs=%s" % nInputs)
+        #self.debug("nInTot=%s" % nInTot)
+        #self.debug('sizePen=%s' %sizePen)
+
+        # TODO
+        deviationPen = bn.nansum(diff) / 2. # to be in agreement with CNO but wrong
+        self.diff = diff / 2.
+        #self.debug("deviationPen=%s"% deviationPen)
+        #self.debug("Nna=%s"% Nna)
+        #self.debug("nDataP=%s"% nDataP)
+
+        deviationPen  /= float(nDataP)
+        #self.debug("deviationPen=%s"% deviationPen)
+        S = deviationPen + sizePen / nDataP
+        return S
+
+    def plot_errors(self, columns=None):
+        # What do we use here: self.values
+        print("Use only time 1..")
+
+        # use eval_func with debug one
+        debug = self.debug
+        self.debug = True
+        buffering = self.buffering
+        self.buffering = False
+        self.eval_func(self.ga.results['Best_bitString'][-1])
+        self.buffering = buffering
+        self.debug = debug
+
+        if columns is None:
+            columns = self.data.df.columns
+        X1 = pd.DataFrame(self.debug_values[-1])[columns].copy()
+        X1 = self.get_df()
+        N = X1.shape[0]
+
+        X1['time'] = [self.time] * N
+        X1['cell'] = [self.data.cellLine] * N
+        X1['experiment'] = self.data.experiments.index
+        X1.set_index(['cell', 'experiment', 'time'], inplace=True)
+        self.data.sim.ix[X1.index] = X1
+        self.data.plot(mode='mse')
+        print("MSE= %s(caspo/cno with only 1 time)" % self.score())
+        print("MSE= %s(cellnoptr with only 1 time)" % str(self.score()/2.))
+
+
+    #@do_profile()
+    def eval_func(self, chromosome, prior=[]):
+        """
+
+        :param prior: a list of same length as chromosome made of 0/1/None
+
+        """
+        # TODO limnit the buffering ?
+        for i, this in enumerate(prior):
+            if this is not None:
+                chromosome[i] = this
+        
+        # using string or tuple takes about the same time but faster than a list
+        str_chrome = tuple(chromosome)
+
+        if self.buffering and len(self.buffer)<self.length_buffer and str_chrome in self.buffer.keys():
+            return self.buffer[str_chrome]
+        else:
+            # 110 times faster using numpy array instead of a list...
+            reactions = [x for c,x in zip(chromosome, self._np_reactions) if c==1]
+            self.simulate(reactions=reactions)
+            score = self.score()
+            if self.buffering is True and len(self.buffer)<self.length_buffer:
+                self.buffer[str_chrome] = score
+            self.counter +=1
+            return score
+
+    def optimise(self, verbose=False, maxgens=500, show=False, reltol=0.1, 
+        maxtime=60, prior=[]):
+        """Using the CellNOptR-like GA"""
+        from cno.optimisers import genetic_algo
+        ga = genetic_algo.GABinary(len(self.model.reactions), verbose=verbose, 
+                maxgens=maxgens, maxtime=maxtime, reltol=reltol)
+
+        def eval_func_in(x):
+            return self.eval_func(x, prior=prior)
+
+        self.counter = 0
+        ga.getObj = eval_func_in
+        ga.run(show=show)
+        self.ga = ga
+        self._fill_results()
+        return ga
+
+    def _fill_results(self):
+        from easydev import AttrDict
+        res = AttrDict(**self.ga.results)
+
+
+        results = pd.DataFrame(self.ga.results)
+        columns_int = ['Generation', 'Stall_Generation']
+        columns_float = ['Best_score', 'Avg_Score_Gen', 'Best_Score_Gen', 'Iter_time']
+        results[columns_int] = results[columns_int].astype(int)
+        results[columns_float] = results[columns_float].astype(float)
+
+
+        results = {
+                'best_score': res.Best_score,
+                'best_bitstring': res.Best_bitString[-1],
+                'all_scores': self.ga.popTolScores,
+                'all_bitstrings': self.ga.popTol,
+                'reactions': self.model.reactions,
+                #'sim_results': self.session.sim_results,  # contains mse and sim at t0,t1,
+                'results': results,
+                #'models': models,
+                #'stimuli': self.session.stimuli.copy(),
+                #'inhibitors': self.session.inhibitors.copy(),
+                #'species': self.session.species,
+        }
+
+        results['pkn'] = self.pknmodel
+        results['midas'] = self.data
+        #self.results.models = models
+        all_bs = self.ga.popTol
+        df = pd.DataFrame(all_bs, columns=self.model.reactions)
+        models = BooleanModels(df)
+        models.scores = results['all_scores']
+
+        self.results.results = results
+        self.results.models = models
 
 
 
 
-
-
-
-class Fuzzy(object):
-    def __init__(self):
-        pass
