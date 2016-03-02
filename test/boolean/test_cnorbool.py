b1fd2ea4aefffaeff11c5ec5f27b83b382dfe54d
Thomas Cokelaer <cokelaer@gmail.com>
2016-03-02 14:30:01 +0100

    update test.

--- test/boolean/test_cnorbool.py
+++ test/boolean/test_cnorbool.py
@@ -2,6 +2,9 @@ from cno.boolean.cnorbool import CNORbool
 from cno import cnodata
 
 
+from nose.plugins.attrib import attr
+
+@attr('skip_travis')
 def test_cnorbool():
     c = CNORbool(cnodata("PKN-ToyMMB.sif"), cnodata("MD-ToyMMB.csv"))
     c.optimise(maxgens=3, popsize=10)
